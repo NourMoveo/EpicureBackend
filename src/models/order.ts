5dfe4f0e9623104da5cb3a408bcf1873b87736f4
NourEdres <nouruni410@gmail.com>
2024-03-13 10:40:52 +0200

    new changes

--- src/models/order.ts
+++ src/models/order.ts
@@ -2,10 +2,17 @@ import mongoose, { Schema, Document } from "mongoose";
 import { DishI } from "./dish";
 import { RestaurantI } from "./restaurant";
 
+interface OrderDish {
+  count: number;
+  dishes: DishI;
+  side: string[];
+  changes: string[];
+}
+
 export interface OrderI extends Document {
   date: Date;
   total: number;
-  dishesPerQuantity: { count: number; dishes: DishI }[];
+  dishesPerQuantity: OrderDish[];
   arrivingTime: number;
   restaurant: RestaurantI;
 }
@@ -16,11 +23,13 @@ const OrderSchema: Schema = new Schema({
   dishesPerQuantity: [
     {
       count: { type: Number, required: true },
-      dishes: { type: Schema.Types.ObjectId}
+      dishes: { type: Schema.Types.ObjectId, ref: "Dish", required: true },
+      side: { type: [String], default: [] },
+      changes: { type: [String], default: [] }
     }
   ],
   arrivingTime: { type: Number, required: true },
-  restaurant: { type: Schema.Types.ObjectId, ref: "Restaurant" }
+  restaurant: { type: Schema.Types.ObjectId, ref: "Restaurant", required: true }
 });
 
 const Order = mongoose.model<OrderI>("Order", OrderSchema);

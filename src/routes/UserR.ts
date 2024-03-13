4a00bdd3f0819b49d4e7cfa6c2d2b5071f0a9c07
NourEdres <nouruni410@gmail.com>
2024-03-13 10:34:03 +0200

    new changes

--- src/routes/UserR.ts
+++ src/routes/UserR.ts
@@ -1,6 +1,7 @@
 import express, { Router } from "express";
 import UserController from "../controllers/UserC";
-import UserModel from "../models/user"; // Adjust the path if necessary
+import UserModel from "../models/user";
+import { authenticateUser, verifyToken } from "../middleware/userAuthentication";
 
 const userRouter = Router();
 const userController = new UserController(UserModel);
@@ -10,24 +11,28 @@ userRouter.post("/", (req, res) => {
     userController.create(req, res);
 });
 
-// Route to get all users
+// Route to authenticate user and issue JWT token
+userRouter.post("/login", authenticateUser);
+
+// Protected routes - require valid JWT token
+userRouter.use(verifyToken);
+
+// Route to get all users (protected)
 userRouter.get("/", (req, res) => {
     userController.getAll(req, res);
 });
 
-// Route to update a user
+// Route to update a user (protected)
 userRouter.put("/:id", (req, res) => {
     userController.update(req, res);
 });
 
-// Route to delete a user
+// Route to delete a user (protected)
 userRouter.delete("/:id", (req, res) => {
     userController.delete(req, res);
 });
 
-
-
-// Route to get user by ID
+// Route to get user by ID (protected)
 userRouter.get("/:id", (req, res) => {
     userController.getById(req, res);
 });
